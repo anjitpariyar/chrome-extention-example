866531c1761a4f27d8ccbce8c4999b12fb2c7643
anjitpariyar <anjitpariyar@gmail.com>
2022-04-10 09:42:35 +0545

    appedn click me

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log("background js");
+//attempt to know the load of the page is done
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == "complete") {
+    // do your things
+    chrome.tabs.sendMessage(tabId, { load: "complete" });
+  }
+});
--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-// All about observing the loading state
-const loaderparent = document.querySelector(".next-loading");
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const mutationCallback = function (mutationsList, observer) {
-  console.log("mutationsList", mutationsList);
-  console.log("observer", observer);
-  // Use traditional 'for loops' for IE 11
-  for (const mutation of mutationsList) {
-    //
-    if (mutation.type === "childList") {
-      // not having loading class means data is already appended
-      if (!loaderparent.classList.contains("loading")) {
-        // disconnecting the observer
-        debugger;
-        cardParentGrab();
-      }
-    }
-    // only if you need to check on attribute change
-    // else if (mutation.type === "attributes") {
-    //   console.log("The " + mutation.attributeName + " attribute was modified.");
-    // }
-  }
+// append button on parent
+const Button = (parent) => {
+  const button = document.createElement("button");
+  button.setAttribute("class", "append-button");
+  button.textContent = "Click Me";
+  console.log(button);
+  console.log(parent);
+  // debugger;
+  parent.appendChild(button);
 };
 
-// Create an observer instance linked to the callback function
-const mutationObserver = new MutationObserver(mutationCallback);
-
-// Start observing the target node for configured mutations
-mutationObserver.observe(loaderparent, config);
-// end of observation function
-
-// function that grab the card parent
-let cardParent;
-const cardParentGrab = () => {
-  mutationObserver.disconnect();
-  mutationObserver.takeRecords();
-  // no repeat assign
-  if (!cardParent) {
-    cardParent = document.querySelectorAll(
-      ".next-loading .list .b2b-ocms-fusion-comp .offer-img"
-    );
-    console.log("parent", cardParent);
-  }
+const Start = () => {
+  // .next-loading .list .b2b-ocms-fusion-comp .offer-img
+  const parents = document.querySelectorAll(".ch-main-nav.fd-clr");
+  [...parents].forEach((parent) => {
+    Button(parent);
+  });
 };
+
+// It reccieve message after fully loaded
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  Start();
+});
