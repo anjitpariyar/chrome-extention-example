3bc1c7763247c5c12d1c40bbc5c6c86e9c5f4888
anjitpariyar <anjitpariyar@gmail.com>
2022-04-11 08:16:32 +0545

    add hi

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
 //attempt to know the load of the page is done
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == "complete") {
+chrome?.tabs?.onUpdated?.addListener(function (tabId, changeInfo) {
+  if (changeInfo?.status == "complete") {
     // do your things
-    chrome.tabs.sendMessage(tabId, { load: "complete" });
+    // chrome?.tabs?.sendMessage(tabId, { load: "complete" });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { greeting: "hello" },
+        function (response) {
+          console.log(response.farewell);
+        }
+      );
+    });
   }
 });
--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 // append button on parent
+console.log("start");
+
+const Alert = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  alert("Hello");
+};
+
 const Button = (parent) => {
   const button = document.createElement("button");
   button.setAttribute("class", "append-button");
   button.textContent = "Click Me";
-  console.log(button);
-  console.log(parent);
-  // debugger;
+  button.addEventListener("click", (e) => {
+    Alert(e);
+  });
+  debugger;
   parent.appendChild(button);
 };
 
 const Start = () => {
   // .next-loading .list .b2b-ocms-fusion-comp .offer-img
-  const parents = document.querySelectorAll(".ch-main-nav.fd-clr");
+  debugger;
+  const parents = document.querySelectorAll(".offer-about");
+  console.log("parents", parents);
   [...parents].forEach((parent) => {
     Button(parent);
   });
 };
 
 // It reccieve message after fully loaded
-chrome.runtime.onMessage.addListener(function (request, sender) {
+// chrome.runtime.onMessage.addListener(function (request, sender) {
+//   Start();
+//   console.log("reccive");
+// });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? "from a content script:" + sender.tab.url
+      : "from the extension"
+  );
   Start();
+  if (request.greeting === "hello") sendResponse({ farewell: "goodbye" });
 });
+// https://show.1688.com/*
